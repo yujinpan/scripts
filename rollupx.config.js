49437d94b0fcd44c838001e1cf173576440e7ea7
yujinpan <1192878390@qq.com>
2023-10-12 17:25:27 +0800

    Initial from node-base

---
+++ rollupx.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  banner:
+    '/*!\n' +
+    ` * scripts ${require('./package.json').version}\n` +
+    ` * (c) 2023-${new Date().getFullYear()}\n` +
+    ' */\n',
+
+  aliasConfig: {
+    '@': 'src',
+  },
+
+  outputDir: 'lib',
+
+  formats: [
+    {
+      format: 'cjs',
+      inputFiles: ['**/*'],
+      outputDir: 'lib',
+      outputFile: '[name][ext]',
+    },
+  ],
+
+  typesOutputDir: 'types',
+
+  node: true,
+};
