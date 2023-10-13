9b9c27bd7a85c590644e1c134b5b406e36a1102e
yujinpan <1192878390@qq.com>
2023-10-13 10:12:56 +0800

    feat: add subscribes transition script for justmysocks to quantumultx

---
+++ src/justmysocks-to-quantumultx.ts
@@ -0,0 +1,85 @@
+const { $resource, $done } = Function('return this')() as {
+  $resource: { content: string };
+  $done: (data: { content: string }) => any;
+};
+
+if ($resource) {
+  $done({
+    content: transform($resource.content),
+  });
+}
+
+export function transform(content: string): string {
+  const data: Server[] = readBase64(content)
+    .split('\n')
+    .map((item) => {
+      const [protocol, infos] = item.split('://');
+
+      try {
+        return {
+          ss: readShadowsocks,
+          vmess: readVmess,
+        }[protocol]?.(infos);
+      } catch (e) {
+        //
+      }
+    })
+    .filter((item) => !!item);
+
+  return data
+    .map((item) =>
+      Object.entries(item)
+        .map(([key, val]) => `${key}=${val}`)
+        .join(', '),
+    )
+    .join('\n');
+}
+
+type Server = {
+  shadowsocks?: string;
+  vmess?: string;
+  method: string;
+  password: string;
+  tag: string;
+};
+
+function readVmess(str: string): Server {
+  const { ps, port, id, add } = readJson(readBase64(str));
+
+  return {
+    vmess: `${add}:${port}`,
+    method: 'aes-128-gcm',
+    password: id,
+    tag: ps,
+  };
+}
+
+function readShadowsocks(str: string): Server {
+  const [infoBase64, tag] = str.split('#');
+  const info = readBase64(infoBase64);
+  const [method, password, ip, port] = info.split(/[:@]/);
+
+  return {
+    shadowsocks: `${ip}:${port}`,
+    method,
+    password,
+    tag,
+  };
+}
+
+function readJson(json: string) {
+  try {
+    return JSON.parse(json);
+  } catch (e) {
+    return json;
+  }
+}
+
+function readBase64(base64: string) {
+  return new TextDecoder().decode(base64ToBytes(base64));
+}
+
+function base64ToBytes(base64: string) {
+  const binString = atob(base64);
+  return Uint8Array.from(binString, (m) => m.codePointAt(0));
+}
