520a4249b8d00a0638cd0df2d4d2842fe1b28185
yujinpan <1192878390@qq.com>
2023-10-13 17:24:45 +0800

    remove unused

--- src/index.spec.ts
+++
@@ -1,7 +0,0 @@
-import { helloWorld } from './index';
-
-describe('helloWorld', () => {
-  it('should say', function () {
-    expect(helloWorld({ msg: 'Hello, World!' })).toBe('Hello, World!');
-  });
-});
--- src/index.ts
+++
@@ -1,5 +0,0 @@
-export function helloWorld(options: { msg: string }) {
-  // eslint-disable-next-line no-console
-  console.log(options.msg);
-  return options.msg;
-}
